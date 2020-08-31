3f5beb9fb38038c04d58fe05fdddf0a4a355d7e4
goranaviani <goranaviani@gmail.com>
2020-08-31 16:45:32 +0200

    finishing up

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import kivy
-kivy.require('1.11.1') # replace with your current kivy version !
-
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
-from random import randint
+
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
@@ -22,20 +18,15 @@ class PongPaddle(Widget):
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
 
-class PongBall(Widget):
 
-    # velocity of the ball on x and y axis
+class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
-
-    # referencelist property so we can use ball.velocity as
-    # a shorthand, just like e.g. w.pos for w.x and w.y
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    # ``move`` function will move the ball one step. This
-    #  will be called in equal intervals to animate the ball
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
+
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
@@ -46,17 +37,16 @@ class PongGame(Widget):
         self.ball.center = self.center
         self.ball.velocity = vel
 
-
     def update(self, dt):
         self.ball.move()
 
-        # bounce off top and bottom
-        if (self.ball.y < 0) or (self.ball.top > self.height):
-            self.ball.velocity_y *= -1
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
-        # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        # bounce ball off bottom or top
+        if (self.ball.y < self.y) or (self.ball.top > self.top):
+            self.ball.velocity_y *= -1
 
         # went of to a side to score point?
         if self.ball.x < self.x:
@@ -72,10 +62,11 @@ class PongGame(Widget):
         if touch.x > self.width - self.width / 3:
             self.player2.center_y = touch.y
 
-class PongApp(App):
 
+class PongApp(App):
     def build(self):
         game = PongGame()
+        game.serve_ball()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 
